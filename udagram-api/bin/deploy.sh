3ce889466405de287c0b8b0efa791fc7637e9501
osomaher2020 <osomaher1@gmail.com>
2022-04-30 20:19:03 +0200

    make setenv multiline for a screenshot

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,14 @@
 eb init osomaher-app --platform node.js --region us-east-1
 eb use Osomaherapp-env
-eb setenv PORT='3000' DB_PORT='5432' POSTGRES_HOST='osomaherdb.c9cgcbjfdejg.us-east-1.rds.amazonaws.com' POSTGRES_DB='osomaherdb' POSTGRES_USERNAME='osomaher' POSTGRES_PASSWORD='osomaher123' JWT_SECRET='sola123' AWS_REGION='us-east-1' AWS_ACCESS_KEY_ID='AKIA2BJZBEZOZ6BLQGVI' AWS_SECRET_ACCESS_KEY='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' AWS_BUCKET='elasticbeanstalk-us-east-1-689998734941'
+
+eb setenv PORT='3000'
+eb setenv DB_PORT='5432'
+eb setenv POSTGRES_HOST='osomaherdb.c9cgcbjfdejg.us-east-1.rds.amazonaws.com'
+eb setenv POSTGRES_DB='osomaherdb'
+eb setenv POSTGRES_USERNAME='osomaher'
+eb setenv POSTGRES_PASSWORD='osomaher123'
+eb setenv JWT_SECRET='sola123'
+eb setenv AWS_REGION='us-east-1'
+eb setenv AWS_BUCKET='elasticbeanstalk-us-east-1-689998734941'
+
 eb deploy Osomaherapp-env
